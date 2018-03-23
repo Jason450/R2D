0891056b3ccda160e6284d4fdd68d2146e1f2e51
Jason <jasonchiewsam@gmail.com>
2018-03-23 11:34:02 +0100

    Logo Screen Implemented
    Implemented Logo Screen with transition to Title Screen

--- Assets/Scripts/Managers/GameManagerScript.cs
+++ Assets/Scripts/Managers/GameManagerScript.cs
@@ -18,7 +18,7 @@ public class GameManagerScript : MonoBehaviour
 
     public void LoadGameplay()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void QuitGame()
---
+++ Assets/Scripts/Managers/LogoScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LogoScreen : MonoBehaviour
+{
+    public float counter;
+    public Image image;
+
+	void Start ()
+    {
+        counter = 3;
+        image.color = new Vector4(image.color.r, image.color.g, image.color.b, image.color.a);
+    }
+
+    void Update ()
+    {
+        counter -= Time.deltaTime;
+
+        if (counter <= 0 && image.color.a >= 1)
+        {
+            counter = 0;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
