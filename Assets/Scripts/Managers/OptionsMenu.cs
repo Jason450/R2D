8a74d44fd68bf63a46e40d1af881fdfe95369cb1
Jason <jasonchiewsam@gmail.com>
2018-04-16 17:13:42 +0200

    Options Menu Update

--- Assets/Scripts/Managers/OptionsMenu.cs
+++ Assets/Scripts/Managers/OptionsMenu.cs
@@ -20,13 +20,21 @@ public class OptionsMenu : MonoBehaviour
     public void Resolution(int res)
     {
         if(res == 1)
-        {            Debug.Log("aaa");
+        {
+            Debug.Log("aaa");
             Screen.SetResolution(1280, 720, Screen.fullScreen);
             Debug.Log(quality);
 
         }
-        if(res == 2) Screen.SetResolution(1600, 1200, Screen.fullScreen);
-        if(res == 3) Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        if(res == 2)
+        {
+            Debug.Log("aaa2");
+            Screen.SetResolution(1600, 1200, Screen.fullScreen);
+        }
+        if(res == 3)
+        {
+            Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        }
     }
 
     public void Quality(int qua)
